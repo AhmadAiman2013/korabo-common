91fa9b6beda4997b60a3e7540fe6b8678f39f1ce
ahmad aiman <ahmadaimanalias@gmail.com>
2026-02-26 13:30:12 +0800

    fix: remove necessary claim

--- claims/src/lib.rs
+++ claims/src/lib.rs
@@ -5,9 +5,7 @@ pub struct Claims {
     pub sub: String,
     pub iat: i64,
     pub exp: i64,
-    pub nbf: i64,
     pub jti: String,
     pub iss: String,
     pub aud: String,
-    pub scope: String,
 }
