8a2f8797127767db81e93c88480a74c0f21cc172
ahmad aiman <ahmadaimanalias@gmail.com>
2026-02-26 10:03:28 +0800

    feat: add more claims

--- claims/src/lib.rs
+++ claims/src/lib.rs
@@ -5,7 +5,9 @@ pub struct Claims {
     pub sub: String,
     pub iat: i64,
     pub exp: i64,
+    pub nbf: i64,
     pub jti: String,
     pub iss: String,
     pub aud: String,
+    pub scope: String,
 }
